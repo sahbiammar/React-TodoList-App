4d0974632623924f638ea7d923a50a351f8620e0
sahbiammar <sahbi.ammar.1995@gmail.com>
2020-09-07 00:21:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'; 
 import './App.css';
+import TodoItmes from './Component/TodoItems/TodoItems'
+import AddItems from './Component/AddItem/AddItems'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    items : [
+      {id:1,name:'Logan',age:22},
+      {id:2,name:'Kenzie',age:23},
+      {id:3,name:'Elon',age:24}, 
+    ]
+  }
+
+  deleteItmes = (id) => {
+    let items = this.state.items.filter(item => {
+      return item.id !== id
+    })
+    this.setState({items})
+  }
+
+  addItem = (item) => { 
+    item.id = Math.random();
+    let items = this.state.items;
+    items.push(item);
+    this.setState({items})
+  }
+
+  render() {
+    return (
+        <div className="App container">
+            <h1 className="text-center">TodoList App / not responsive   </h1> 
+            <TodoItmes items={this.state.items} deleteItem={this.deleteItmes}/>
+            <AddItems addItem={this.addItem}/>
+        </div>
+    );
+  }
+
 }
 
 export default App;
